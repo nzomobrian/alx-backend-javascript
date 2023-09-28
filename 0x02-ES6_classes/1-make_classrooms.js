d01b896e4fd67eb90d8e2f6cc2a793ce1688b688
Nzomo <brnzomo17@gmail.com>
2023-09-28 22:24:30 +0300

    This function Imports the ClassRoom class from 0-classroom.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+/**
+ * Creates an array of ClassRooms with a specific size.
+ * @returns An array of link ClassRooms.
+ */
+export default function initializeRooms()
+{
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
