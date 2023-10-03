3886eb8d9dc0965c12c4b0977880cc5e6afde304
Nzomo <brnzomo17@gmail.com>
2023-10-04 01:51:59 +0300

    function that returns an updated map for all items with initial quantity at 1.

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -5,12 +5,15 @@
  */
 export default function updateUniqueItems(map)
 {
-  if (!(map instanceof Map)) {
+  if (!(map instanceof Map))
+	{
     throw new Error('Cannot process');
-  }
-  map.forEach((value, key) => {
-    if (value === 1) {
+	}
+  map.forEach((value, key) =>
+	  {
+    if (value === 1)
+		  {
       map.set(key, 100);
-    }
-  });
+		  }
+	  });
 }
